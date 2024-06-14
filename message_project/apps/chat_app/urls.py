ce7cf402fdd207505cb079a69e168937e5042d44
Ajay Parihar <ajayparihar876@gmail.com>
2024-06-14 16:39:08 +0530

    add css to login register

--- message_project/apps/chat_app/urls.py
+++ message_project/apps/chat_app/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('group_chat/<str:group_name>/', views.group_chat, name='group_chat'),
     path('personal_chat/<str:friend_username>/', views.personal_chat, name='personal_chat'),
 
-    path('login/', views.user_login, name='user_login'),
+    path('', views.user_login, name='user_login'),
     path('logout/', views.user_logout, name='user_logout'),
     path('register/', views.user_register, name='user_register'),
 
